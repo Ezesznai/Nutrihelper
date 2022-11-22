1087c48de89a3e18f4666ad6b3a5d2db2121ffcc
viceenn <108874812+viceenn@users.noreply.github.com>
2022-11-22 10:26:05 -0300

    TP BENJA

---
+++ TP HARRY POTTER BENJAMIN MENGOCHEA/TP HARRY POTTER BENJAMIN MENGOCHEA/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace TP_HARRY_POTTER_BENJAMIN_MENGOCHEA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAnotacion1 = new System.Windows.Forms.Button();
+            this.btnSnitch2 = new System.Windows.Forms.Button();
+            this.lblEq1 = new System.Windows.Forms.Label();
+            this.lblPuntaje1 = new System.Windows.Forms.Label();
+            this.btnSnitch1 = new System.Windows.Forms.Button();
+            this.btnAnotacion2 = new System.Windows.Forms.Button();
+            this.btnFinTorneo = new System.Windows.Forms.Button();
+            this.lblPuntaje2 = new System.Windows.Forms.Label();
+            this.lblEq2 = new System.Windows.Forms.Label();
+            this.txtEq1 = new System.Windows.Forms.TextBox();
+            this.txtEq2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAnotacion1
+            // 
+            this.btnAnotacion1.Location = new System.Drawing.Point(83, 281);
+            this.btnAnotacion1.Name = "btnAnotacion1";
+            this.btnAnotacion1.Size = new System.Drawing.Size(87, 23);
+            this.btnAnotacion1.TabIndex = 0;
+            this.btnAnotacion1.Text = "ANOTACION";
+            this.btnAnotacion1.UseVisualStyleBackColor = true;
+            this.btnAnotacion1.Click += new System.EventHandler(this.btnAnotacion1_Click);
+            // 
+            // btnSnitch2
+            // 
+            this.btnSnitch2.Location = new System.Drawing.Point(980, 368);
+            this.btnSnitch2.Name = "btnSnitch2";
+            this.btnSnitch2.Size = new System.Drawing.Size(113, 39);
+            this.btnSnitch2.TabIndex = 1;
+            this.btnSnitch2.Text = "CAPTURA SNITCH";
+            this.btnSnitch2.UseVisualStyleBackColor = true;
+            this.btnSnitch2.Click += new System.EventHandler(this.btnSnitch2_Click);
+            // 
+            // lblEq1
+            // 
+            this.lblEq1.AutoSize = true;
+            this.lblEq1.Location = new System.Drawing.Point(99, 60);
+            this.lblEq1.Name = "lblEq1";
+            this.lblEq1.Size = new System.Drawing.Size(57, 13);
+            this.lblEq1.TabIndex = 2;
+            this.lblEq1.Text = "EQUIPO 1";
+            this.lblEq1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblPuntaje1
+            // 
+            this.lblPuntaje1.AutoSize = true;
+            this.lblPuntaje1.Location = new System.Drawing.Point(89, 148);
+            this.lblPuntaje1.Name = "lblPuntaje1";
+            this.lblPuntaje1.Size = new System.Drawing.Size(13, 13);
+            this.lblPuntaje1.TabIndex = 3;
+            this.lblPuntaje1.Text = "0";
+            // 
+            // btnSnitch1
+            // 
+            this.btnSnitch1.Location = new System.Drawing.Point(83, 334);
+            this.btnSnitch1.Name = "btnSnitch1";
+            this.btnSnitch1.Size = new System.Drawing.Size(87, 37);
+            this.btnSnitch1.TabIndex = 4;
+            this.btnSnitch1.Text = "CAPTURA SNITCH";
+            this.btnSnitch1.UseVisualStyleBackColor = true;
+            this.btnSnitch1.Click += new System.EventHandler(this.btnSnitch1_Click);
+            // 
+            // btnAnotacion2
+            // 
+            this.btnAnotacion2.Location = new System.Drawing.Point(980, 305);
+            this.btnAnotacion2.Name = "btnAnotacion2";
+            this.btnAnotacion2.Size = new System.Drawing.Size(82, 23);
+            this.btnAnotacion2.TabIndex = 5;
+            this.btnAnotacion2.Text = "ANOTACION";
+            this.btnAnotacion2.UseVisualStyleBackColor = true;
+            this.btnAnotacion2.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnFinTorneo
+            // 
+            this.btnFinTorneo.Location = new System.Drawing.Point(585, 281);
+            this.btnFinTorneo.Name = "btnFinTorneo";
+            this.btnFinTorneo.Size = new System.Drawing.Size(107, 35);
+            this.btnFinTorneo.TabIndex = 6;
+            this.btnFinTorneo.Text = "FINALIZAR TORNEO";
+            this.btnFinTorneo.UseVisualStyleBackColor = true;
+            // 
+            // lblPuntaje2
+            // 
+            this.lblPuntaje2.AutoSize = true;
+            this.lblPuntaje2.Location = new System.Drawing.Point(988, 183);
+            this.lblPuntaje2.Name = "lblPuntaje2";
+            this.lblPuntaje2.Size = new System.Drawing.Size(35, 13);
+            this.lblPuntaje2.TabIndex = 7;
+            this.lblPuntaje2.Text = "label3";
+            // 
+            // lblEq2
+            // 
+            this.lblEq2.AutoSize = true;
+            this.lblEq2.Location = new System.Drawing.Point(988, 96);
+            this.lblEq2.Name = "lblEq2";
+            this.lblEq2.Size = new System.Drawing.Size(57, 13);
+            this.lblEq2.TabIndex = 8;
+            this.lblEq2.Text = "EQUIPO 2";
+            // 
+            // txtEq1
+            // 
+            this.txtEq1.Location = new System.Drawing.Point(72, 105);
+            this.txtEq1.Name = "txtEq1";
+            this.txtEq1.Size = new System.Drawing.Size(100, 20);
+            this.txtEq1.TabIndex = 9;
+            this.txtEq1.TextChanged += new System.EventHandler(this.txtEq1_TextChanged);
+            // 
+            // txtEq2
+            // 
+            this.txtEq2.Location = new System.Drawing.Point(980, 132);
+            this.txtEq2.Name = "txtEq2";
+            this.txtEq2.Size = new System.Drawing.Size(100, 20);
+            this.txtEq2.TabIndex = 10;
+            this.txtEq2.TextChanged += new System.EventHandler(this.txtEq2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1469, 683);
+            this.Controls.Add(this.txtEq2);
+            this.Controls.Add(this.txtEq1);
+            this.Controls.Add(this.lblEq2);
+            this.Controls.Add(this.lblPuntaje2);
+            this.Controls.Add(this.btnFinTorneo);
+            this.Controls.Add(this.btnAnotacion2);
+            this.Controls.Add(this.btnSnitch1);
+            this.Controls.Add(this.lblPuntaje1);
+            this.Controls.Add(this.lblEq1);
+            this.Controls.Add(this.btnSnitch2);
+            this.Controls.Add(this.btnAnotacion1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAnotacion1;
+        private System.Windows.Forms.Button btnSnitch2;
+        private System.Windows.Forms.Label lblEq1;
+        private System.Windows.Forms.Label lblPuntaje1;
+        private System.Windows.Forms.Button btnSnitch1;
+        private System.Windows.Forms.Button btnAnotacion2;
+        private System.Windows.Forms.Button btnFinTorneo;
+        private System.Windows.Forms.Label lblPuntaje2;
+        private System.Windows.Forms.Label lblEq2;
+        private System.Windows.Forms.TextBox txtEq1;
+        private System.Windows.Forms.TextBox txtEq2;
+    }
+}
+
---
+++ TP HARRY POTTER BENJAMIN MENGOCHEA/TP HARRY POTTER BENJAMIN MENGOCHEA/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_HARRY_POTTER_BENJAMIN_MENGOCHEA
+{
+    public partial class Form1 : Form
+    {
+        public char Eq1;
+        public char Eq2;
+        public int Puntaje1;
+        public int Puntaje2;
+        public int PGG;
+        public int PGH;
+        public int PGR;
+        public int PGS;
+        public bool Snitch1 = false;
+        public bool Snitch2 = false;
+        public char[] Equipos;
+        public string GanadorP;
+        public string GanadorT;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Puntaje2 += 10;
+        }
+
+        private void txtEq1_TextChanged(object sender, EventArgs e)
+        {
+            Eq1 = Convert.ToChar(txtEq1.Text);
+        }
+
+        private void txtEq2_TextChanged(object sender, EventArgs e)
+        {
+            Eq2 = Convert.ToChar(txtEq2.Text);
+        }
+
+        private void btnAnotacion1_Click(object sender, EventArgs e)
+        {
+            Puntaje1 += 10;
+        }
+
+        private void btnSnitch1_Click(object sender, EventArgs e)
+        {
+            Puntaje1 += 150;
+            Snitch1 = true;
+            if (Puntaje1 > Puntaje2)
+            {
+                Main(Equipos, Eq1, PGG, PGH, PGR, PGS);
+            }
+            
+
+        }
+
+        private void btnSnitch2_Click(object sender, EventArgs e)
+        {
+            Puntaje2 += 150;
+            if (Puntaje2 > Puntaje1)
+            {
+                Main(Equipos, Eq2, PGG, PGH, PGR, PGS);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            char[] Equipos = new char[4];
+            Equipos[0] = 'G';
+            Equipos[1] = 'H';
+            Equipos[2] = 'R';
+            Equipos[3] = 'S';
+        }
+        public static void Main(char[] Equipos, char Eq, int PGG, int PGH, int PGR, int PGS)
+        {
+            for (int i = 0;i>3; i++){ 
+            
+            if (Equipos[i] == 'G' && Eq == Equipos[i])
+                {
+                    MessageBox.Show("El ganador del partido es Griffindor");
+                    PGG =+ 1;
+                }
+            else if (Equipos[i] == 'H' && Eq == Equipos[i])
+                {
+                    MessageBox.Show("El ganador del partido es Hufflepuff");
+                    PGH =+ 1;
+                }
+            else if (Equipos[i] == 'R' && Eq == Equipos[i])
+                {
+                    MessageBox.Show("El ganador del partido es Ravenclaw");
+                    PGR =+ 1;
+                }
+            else if (Equipos[i] == 'S' && Eq == Equipos[i])
+                {
+                    MessageBox.Show("El ganador del partido es Slytherin");
+                    PGS =+ 1;
+                }
+
+
+            }
+
+
+
+
+        }
+
+
+    }
+
+    
+}
+
