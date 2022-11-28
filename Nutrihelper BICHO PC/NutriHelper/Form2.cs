227abf606ef01a0a26dfc0537a084f25be323f6c
viceenn <108874812+viceenn@users.noreply.github.com>
2022-11-28 12:41:55 -0300

    Add files via upload

---
+++ Nutrihelper BICHO PC/NutriHelper/Form2.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+
+namespace NutriHelper
+{
+    public partial class Form2 : Form
+
+    {
+        OleDbConnection db;
+       
+
+        public Form2()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Datos.sexo = cmb_sexo.SelectedItem.ToString();
+            if (cmb_sexo.SelectedIndex == 0)
+            {
+                Datos.sexo = "hombre";
+            }
+            else
+            {
+                Datos.sexo = "mujer";
+            }
+
+        }
+        private void ComboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+           Datos.actividad = cmb_actividad.SelectedIndex;
+
+            if (cmb_actividad.SelectedIndex == 0) {
+
+                Datos.actividad = 1.2f;
+            }
+            else if (cmb_actividad.SelectedIndex == 1)
+            {
+                Datos.actividad = 1.375f;
+            }
+            else if (cmb_actividad.SelectedIndex == 2)
+            {
+                Datos.actividad = 1.55f;
+            }
+            else if (cmb_actividad.SelectedIndex == 3)
+            {
+                Datos.actividad = 1.725f;
+            }
+            else
+            {
+                Datos.actividad = 1.9f;
+            }
+        }
+
+        private void Btn_avanzar_Click(object sender, EventArgs e)
+        {
+
+            if (txt_nombre.Text == "" || txt_altura.Text == "" || txt_peso.Text == "" || txt_edad.Text == "" || cmb_sexo.SelectedIndex == -1 || cmb_actividad.SelectedIndex == -1)
+            {
+                MessageBox.Show("Complete todas las casillas para poder avanzar", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+            else
+            {
+
+                Datos.nombre = txt_nombre.Text;
+                Datos.edad = Convert.ToInt32(txt_edad.Text);
+                Datos.peso = Convert.ToSingle(txt_peso.Text);
+                Datos.metros = Datos.altura / 100;
+                Datos.password = txt_contraseña.Text;
+                Datos.altura = Convert.ToInt32(txt_altura.Text);
+
+                /*db.Open();
+               OleDbCommand nombre_fk;
+               nombre_fk = new OleDbCommand("SELECT nombre FROM Usuarios WHERE nombre = '" + Datos.nombre + "'", db);
+                
+
+               
+              OleDbDataAdapter adapter1 = new OleDbDataAdapter(nombre_fk);
+                DataSet dataset_nombre = new DataSet();
+                adapter1.Fill(dataset_nombre);
+                db.Close();
+
+                if (dataset_nombre.Tables[0].Rows.Count == 0)
+                {
+                    
+                    db.Open();
+                    OleDbCommand info;
+                    info = new OleDbCommand("INSERT INTO Usuarios (nombre, sexo, [password], peso, altura, edad, actividad) VALUES ('" + Datos.nombre + "', '" + Datos.sexo + "', '" + Datos.password + "', '" + Datos.peso + "', '" + Datos.altura + "', '" + Datos.edad + "', '" + Datos.actividad + "')", db);
+                    info.ExecuteNonQuery();
+                    db.Close();*/
+                    Form3 objetivos = new Form3();
+                    objetivos.Show();
+                    this.Hide();
+                
+                //else ()
+                {
+                    //MessageBox.Show("ese nick ya esta ocupado, pureba con otro");
+             
+                }
+                
+            
+                
+                
+              //  db.Open();
+              //  OleDbCommand info;
+               // info = new OleDbCommand("INSERT INTO Usuarios (nombre, sexo, [password], peso, altura, edad, actividad) VALUES ('" + Datos.nombre +"', '"+ Datos.sexo +"', '" + Datos.password + "', '"+ Datos.peso +"', '"+ Datos.altura +"', '" + Datos.edad + "', '" + Datos.actividad + "')", db);
+               // info.ExecuteNonQuery();
+              //  db.Close();
+
+
+            }
+
+
+         
+
+           
+
+
+
+            //while (Datos.nombre == "" || (Datos.edad >= 0 && Datos.edad < 150)|| (Datos.altura > 0 && Datos.altura < 250)|| (Datos.peso > 0 && Datos.peso < 700))
+        
+
+            
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    
+
+    private void Txt_edad_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Lbl_edad_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Txt_contraseña_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            //db = new OleDbConnection();
+            //db.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\47653943\\Documents\\GitHub\\nutrihelper 11 8\\Nutrihelper\\nutrihelperDB.accdb";
+
+    }
+
+        private void Txt_altura_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+    
---
+++ Nutrihelper BICHO PC/NutriHelper/Form7.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace NutriHelper
+{
+    partial class Form7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form7));
+            this.txtIntroducirComida = new System.Windows.Forms.TextBox();
+            this.lblTipoComida = new System.Windows.Forms.Label();
+            this.lblIntroducirComida = new System.Windows.Forms.Label();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.lblInfoNutri = new System.Windows.Forms.Label();
+            this.lblComida1 = new System.Windows.Forms.Label();
+            this.lblComida2 = new System.Windows.Forms.Label();
+            this.lblComida3 = new System.Windows.Forms.Label();
+            this.lblComida4 = new System.Windows.Forms.Label();
+            this.lblComida5 = new System.Windows.Forms.Label();
+            this.lblComida6 = new System.Windows.Forms.Label();
+            this.lblComida7 = new System.Windows.Forms.Label();
+            this.lblComida8 = new System.Windows.Forms.Label();
+            this.lblComida9 = new System.Windows.Forms.Label();
+            this.lblComida10 = new System.Windows.Forms.Label();
+            this.lblBusqueda = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lblkcal = new System.Windows.Forms.Label();
+            this.lblfood_description = new System.Windows.Forms.Label();
+            this.txtporcion = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtIntroducirComida
+            // 
+            this.txtIntroducirComida.Location = new System.Drawing.Point(361, 67);
+            this.txtIntroducirComida.Name = "txtIntroducirComida";
+            this.txtIntroducirComida.Size = new System.Drawing.Size(277, 20);
+            this.txtIntroducirComida.TabIndex = 0;
+            // 
+            // lblTipoComida
+            // 
+            this.lblTipoComida.AutoSize = true;
+            this.lblTipoComida.Location = new System.Drawing.Point(13, 13);
+            this.lblTipoComida.Name = "lblTipoComida";
+            this.lblTipoComida.Size = new System.Drawing.Size(0, 13);
+            this.lblTipoComida.TabIndex = 1;
+            this.lblTipoComida.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // lblIntroducirComida
+            // 
+            this.lblIntroducirComida.AutoSize = true;
+            this.lblIntroducirComida.Location = new System.Drawing.Point(118, 30);
+            this.lblIntroducirComida.Name = "lblIntroducirComida";
+            this.lblIntroducirComida.Size = new System.Drawing.Size(0, 13);
+            this.lblIntroducirComida.TabIndex = 2;
+            this.lblIntroducirComida.Click += new System.EventHandler(this.LblIntroducirComida_Click);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(662, 67);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(126, 32);
+            this.btnAceptar.TabIndex = 3;
+            this.btnAceptar.Text = "ACEPTAR";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.BtnAceptar_Click);
+            // 
+            // lblInfoNutri
+            // 
+            this.lblInfoNutri.AutoSize = true;
+            this.lblInfoNutri.BackColor = System.Drawing.Color.Transparent;
+            this.lblInfoNutri.Location = new System.Drawing.Point(358, 160);
+            this.lblInfoNutri.Name = "lblInfoNutri";
+            this.lblInfoNutri.Size = new System.Drawing.Size(31, 13);
+            this.lblInfoNutri.TabIndex = 4;
+            this.lblInfoNutri.Text = "        ";
+            this.lblInfoNutri.Click += new System.EventHandler(this.LblInfoNutri_Click);
+            // 
+            // lblComida1
+            // 
+            this.lblComida1.AutoSize = true;
+            this.lblComida1.Location = new System.Drawing.Point(659, 393);
+            this.lblComida1.Name = "lblComida1";
+            this.lblComida1.Size = new System.Drawing.Size(10, 13);
+            this.lblComida1.TabIndex = 5;
+            this.lblComida1.Text = ".";
+            this.lblComida1.Click += new System.EventHandler(this.LblComida1_Click);
+            // 
+            // lblComida2
+            // 
+            this.lblComida2.AutoSize = true;
+            this.lblComida2.Location = new System.Drawing.Point(659, 176);
+            this.lblComida2.Name = "lblComida2";
+            this.lblComida2.Size = new System.Drawing.Size(10, 13);
+            this.lblComida2.TabIndex = 6;
+            this.lblComida2.Text = ".";
+            this.lblComida2.Click += new System.EventHandler(this.LblComida2_Click);
+            // 
+            // lblComida3
+            // 
+            this.lblComida3.AutoSize = true;
+            this.lblComida3.Location = new System.Drawing.Point(659, 199);
+            this.lblComida3.Name = "lblComida3";
+            this.lblComida3.Size = new System.Drawing.Size(10, 13);
+            this.lblComida3.TabIndex = 7;
+            this.lblComida3.Text = ".";
+            this.lblComida3.Click += new System.EventHandler(this.LblComida3_Click);
+            // 
+            // lblComida4
+            // 
+            this.lblComida4.AutoSize = true;
+            this.lblComida4.Location = new System.Drawing.Point(659, 221);
+            this.lblComida4.Name = "lblComida4";
+            this.lblComida4.Size = new System.Drawing.Size(10, 13);
+            this.lblComida4.TabIndex = 8;
+            this.lblComida4.Text = ".";
+            this.lblComida4.Click += new System.EventHandler(this.LblComida4_Click);
+            // 
+            // lblComida5
+            // 
+            this.lblComida5.AutoSize = true;
+            this.lblComida5.Location = new System.Drawing.Point(659, 246);
+            this.lblComida5.Name = "lblComida5";
+            this.lblComida5.Size = new System.Drawing.Size(10, 13);
+            this.lblComida5.TabIndex = 9;
+            this.lblComida5.Text = ".";
+            this.lblComida5.Click += new System.EventHandler(this.LblComida5_Click);
+            // 
+            // lblComida6
+            // 
+            this.lblComida6.AutoSize = true;
+            this.lblComida6.Location = new System.Drawing.Point(659, 266);
+            this.lblComida6.Name = "lblComida6";
+            this.lblComida6.Size = new System.Drawing.Size(10, 13);
+            this.lblComida6.TabIndex = 10;
+            this.lblComida6.Text = ".";
+            this.lblComida6.Click += new System.EventHandler(this.LblComida6_Click);
+            // 
+            // lblComida7
+            // 
+            this.lblComida7.AutoSize = true;
+            this.lblComida7.Location = new System.Drawing.Point(659, 292);
+            this.lblComida7.Name = "lblComida7";
+            this.lblComida7.Size = new System.Drawing.Size(10, 13);
+            this.lblComida7.TabIndex = 11;
+            this.lblComida7.Text = ".";
+            this.lblComida7.Click += new System.EventHandler(this.LblComida7_Click);
+            // 
+            // lblComida8
+            // 
+            this.lblComida8.AutoSize = true;
+            this.lblComida8.Location = new System.Drawing.Point(659, 319);
+            this.lblComida8.Name = "lblComida8";
+            this.lblComida8.Size = new System.Drawing.Size(10, 13);
+            this.lblComida8.TabIndex = 12;
+            this.lblComida8.Text = ".";
+            this.lblComida8.Click += new System.EventHandler(this.LblComida8_Click);
+            // 
+            // lblComida9
+            // 
+            this.lblComida9.AutoSize = true;
+            this.lblComida9.Location = new System.Drawing.Point(659, 347);
+            this.lblComida9.Name = "lblComida9";
+            this.lblComida9.Size = new System.Drawing.Size(10, 13);
+            this.lblComida9.TabIndex = 13;
+            this.lblComida9.Text = ".";
+            this.lblComida9.Click += new System.EventHandler(this.LblComida9_Click);
+            // 
+            // lblComida10
+            // 
+            this.lblComida10.AutoSize = true;
+            this.lblComida10.Location = new System.Drawing.Point(659, 369);
+            this.lblComida10.Name = "lblComida10";
+            this.lblComida10.Size = new System.Drawing.Size(10, 13);
+            this.lblComida10.TabIndex = 14;
+            this.lblComida10.Text = ".";
+            this.lblComida10.Click += new System.EventHandler(this.LblComida10_Click);
+            // 
+            // lblBusqueda
+            // 
+            this.lblBusqueda.AutoSize = true;
+            this.lblBusqueda.Location = new System.Drawing.Point(358, 30);
+            this.lblBusqueda.Name = "lblBusqueda";
+            this.lblBusqueda.Size = new System.Drawing.Size(67, 13);
+            this.lblBusqueda.TabIndex = 15;
+            this.lblBusqueda.Text = "BUSQUEDA";
+            this.lblBusqueda.Click += new System.EventHandler(this.LblBusqueda_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(722, 3);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 16;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
+            // 
+            // lblkcal
+            // 
+            this.lblkcal.AutoSize = true;
+            this.lblkcal.Location = new System.Drawing.Point(395, 199);
+            this.lblkcal.Name = "lblkcal";
+            this.lblkcal.Size = new System.Drawing.Size(27, 13);
+            this.lblkcal.TabIndex = 17;
+            this.lblkcal.Text = "kcal";
+            this.lblkcal.Click += new System.EventHandler(this.lblkcal_Click);
+            // 
+            // lblfood_description
+            // 
+            this.lblfood_description.AutoSize = true;
+            this.lblfood_description.Location = new System.Drawing.Point(395, 151);
+            this.lblfood_description.Name = "lblfood_description";
+            this.lblfood_description.Size = new System.Drawing.Size(0, 13);
+            this.lblfood_description.TabIndex = 18;
+            // 
+            // txtporcion
+            // 
+            this.txtporcion.Location = new System.Drawing.Point(361, 112);
+            this.txtporcion.Name = "txtporcion";
+            this.txtporcion.Size = new System.Drawing.Size(277, 20);
+            this.txtporcion.TabIndex = 19;
+            this.txtporcion.TextChanged += new System.EventHandler(this.Txtporcion_TextChanged);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(662, 105);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(126, 32);
+            this.btnOK.TabIndex = 20;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.BtnOK_Click);
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.Location = new System.Drawing.Point(337, 246);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(126, 33);
+            this.btnagregar.TabIndex = 21;
+            this.btnagregar.Text = "Agregar";
+            this.btnagregar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(302, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 22;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(558, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 23;
+            // 
+            // Form7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnagregar);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtporcion);
+            this.Controls.Add(this.lblfood_description);
+            this.Controls.Add(this.lblkcal);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.lblBusqueda);
+            this.Controls.Add(this.lblComida10);
+            this.Controls.Add(this.lblComida9);
+            this.Controls.Add(this.lblComida8);
+            this.Controls.Add(this.lblComida7);
+            this.Controls.Add(this.lblComida6);
+            this.Controls.Add(this.lblComida5);
+            this.Controls.Add(this.lblComida4);
+            this.Controls.Add(this.lblComida3);
+            this.Controls.Add(this.lblComida2);
+            this.Controls.Add(this.lblComida1);
+            this.Controls.Add(this.lblInfoNutri);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.lblIntroducirComida);
+            this.Controls.Add(this.lblTipoComida);
+            this.Controls.Add(this.txtIntroducirComida);
+            this.DoubleBuffered = true;
+            this.Name = "Form7";
+            this.Text = "Form7";
+            this.Load += new System.EventHandler(this.Form7_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIntroducirComida;
+        private System.Windows.Forms.Label lblTipoComida;
+        private System.Windows.Forms.Label lblIntroducirComida;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label lblInfoNutri;
+        private System.Windows.Forms.Label lblComida1;
+        private System.Windows.Forms.Label lblComida2;
+        private System.Windows.Forms.Label lblComida3;
+        private System.Windows.Forms.Label lblComida4;
+        private System.Windows.Forms.Label lblComida5;
+        private System.Windows.Forms.Label lblComida6;
+        private System.Windows.Forms.Label lblComida7;
+        private System.Windows.Forms.Label lblComida8;
+        private System.Windows.Forms.Label lblComida9;
+        private System.Windows.Forms.Label lblComida10;
+        private System.Windows.Forms.Label lblBusqueda;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label lblkcal;
+        private System.Windows.Forms.Label lblfood_description;
+        private System.Windows.Forms.TextBox txtporcion;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
